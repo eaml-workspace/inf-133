9adf48f95edaf2bb659218d19dca5e7994021f16
unknown <emonzonl@fcpn.edu.bo>
2024-03-01 23:10:45 -0400

    tarea servidor soap

--- soap_client.py
+++ soap_client.py
@@ -4,3 +4,9 @@ client = Client('http://localhost:8000')
 
 result = client.service.Saludar(nombre="Emanuel")
 print(result)
+
+result = client.service.CadenaPalindromo(cadena="Emanuel")
+print(result)
+
+result = client.service.CadenaPalindromo(numero1=1, numero2=2)
+print(result)
