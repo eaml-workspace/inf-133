18044c577dc3c8fce3fd9277e1a555f6465fc138
unknown <emonzonl@fcpn.edu.bo>
2024-04-22 18:44:09 -0400

    clase 22/04

---
+++ semana 8/mvc/app/controllers/user_controller.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, request, redirect, url_for
+from views import user_view
+from models.user_model import User
+
+user_bp= Blueprint('user', __name__)
+@user_bp.route('/')
+def usuarios():
+    users = User.get_all()
+    return user_view.usuarios(users)
+
+@user_bp.route('/users', methods=['GET','POST'])
+def registro():
+    if request.method == 'POST':
+        # Obtenemos los datos del formulario
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        contrasena = request.form['contrasena']
+        #fecha_nacimiento = request.form['fecha_nacimiento']
+        # Creamos un nuevo usuario
+        user = User(first_name, last_name, email, contrasena)
+        # Guardamos el usuario
+        user.save()
+        # Redirigimos a la vista de usuarios
+        return redirect(url_for('user.usuarios'))
+    # Llamamos a la vista de registro
+    return user_view.registro()
---
+++ semana 8/mvc/app/models/user_model.py
@@ -0,0 +1,27 @@
+from database import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id= db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    contrasena = db.Column(db.String(50), nullable=False)
+    #fecha_nacimiento = db.Column(db.Date(), nullable=False)
+
+    #Inicializa clase User
+    def __init__(self, first_name, last_name, email, contrasena):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email=email
+        self.contrasena=contrasena
+        #self.fecha_nacimiento=fecha_nacimiento
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return User.query.all()
